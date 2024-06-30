8fb3443fd1c6b3a94a1d2ee7ed1e89038ee97312
snowinjelly <broadwellconnor@gmail.com>
2024-06-30 14:49:31 -0500

    container refactor

--- src/components/EducationalExperienceForm.jsx
+++ src/components/EducationalExperienceForm.jsx
@@ -1,4 +1,5 @@
 import FormInput from "./FormInput";
+import FormInputContainer from "./FormInputContainer";
 
 export default function EducationalExperienceForm({
   schoolName,
@@ -8,30 +9,36 @@ export default function EducationalExperienceForm({
 }) {
   return (
     <section className="educational-experience-form-section form-section">
-      <h3>School:</h3>
-      <FormInput
-        type="text"
-        value={schoolName}
-        valueAsStr="schoolName"
-        id="school-name-input"
-        setData={setEducationalExperienceData}
-      />
-      <h3>Study:</h3>
-      <FormInput
-        type="text"
-        value={titleOfStudy}
-        valueAsStr="titleOfStudy"
-        id="title-of-study-input"
-        setData={setEducationalExperienceData}
-      />
-      <h3>Study Date:</h3>
-      <FormInput
-        type="text"
-        value={dateOfStudy}
-        valueAsStr="dateOfStudy"
-        id="date-of-study-input"
-        setData={setEducationalExperienceData}
-      />
+      <FormInputContainer>
+        <h3>School:</h3>
+        <FormInput
+          type="text"
+          value={schoolName}
+          valueAsStr="schoolName"
+          id="school-name-input"
+          setData={setEducationalExperienceData}
+        />
+      </FormInputContainer>
+      <FormInputContainer>
+        <h3>Study:</h3>
+        <FormInput
+          type="text"
+          value={titleOfStudy}
+          valueAsStr="titleOfStudy"
+          id="title-of-study-input"
+          setData={setEducationalExperienceData}
+        />
+      </FormInputContainer>
+      <FormInputContainer>
+        <h3>Study Date:</h3>
+        <FormInput
+          type="text"
+          value={dateOfStudy}
+          valueAsStr="dateOfStudy"
+          id="date-of-study-input"
+          setData={setEducationalExperienceData}
+        />
+      </FormInputContainer>
     </section>
   );
 }
--- src/components/PracticalExperienceForm.jsx
+++ src/components/PracticalExperienceForm.jsx
@@ -1,4 +1,5 @@
 import FormInput from "./FormInput";
+import FormInputContainer from "./FormInputContainer";
 
 export default function PracticalExperienceForm({
   companyName,
@@ -10,46 +11,56 @@ export default function PracticalExperienceForm({
 }) {
   return (
     <section className="practical-experience-form-section form-section">
-      <h3>Company Name:</h3>
-      <FormInput
-        id="company-name-input"
-        type="text"
-        value={companyName}
-        valueAsStr="companyName"
-        setData={setPracticalExperienceData}
-      />
-      <h3>Position:</h3>
-      <FormInput
-        id="position-title-input"
-        type="text"
-        value={positionTitle}
-        valueAsStr="positionTitle"
-        setData={setPracticalExperienceData}
-      />
-      <h3>Main Responsibilities:</h3>
-      <FormInput
-        id="main-responsibilities-input"
-        type="text"
-        value={mainResponsibilities}
-        valueAsStr="mainResponsibilities"
-        setData={setPracticalExperienceData}
-      />
-      <h3>Worked From:</h3>
-      <FormInput
-        id="worked-from-input"
-        type="text"
-        value={workedFrom}
-        valueAsStr="workedFrom"
-        setData={setPracticalExperienceData}
-      />
-      <h3>Worked Until:</h3>
-      <FormInput
-        id="worked-until-input"
-        type="text"
-        value={workedUntil}
-        valueAsStr="workedUntil"
-        setData={setPracticalExperienceData}
-      />
+      <FormInputContainer>
+        <h3>Company Name:</h3>
+        <FormInput
+          id="company-name-input"
+          type="text"
+          value={companyName}
+          valueAsStr="companyName"
+          setData={setPracticalExperienceData}
+        />
+      </FormInputContainer>
+      <FormInputContainer>
+        <h3>Position:</h3>
+        <FormInput
+          id="position-title-input"
+          type="text"
+          value={positionTitle}
+          valueAsStr="positionTitle"
+          setData={setPracticalExperienceData}
+        />
+      </FormInputContainer>
+      <FormInputContainer>
+        <h3>Main Responsibilities:</h3>
+        <FormInput
+          id="main-responsibilities-input"
+          type="text"
+          value={mainResponsibilities}
+          valueAsStr="mainResponsibilities"
+          setData={setPracticalExperienceData}
+        />
+      </FormInputContainer>
+      <FormInputContainer>
+        <h3>Worked From:</h3>
+        <FormInput
+          id="worked-from-input"
+          type="text"
+          value={workedFrom}
+          valueAsStr="workedFrom"
+          setData={setPracticalExperienceData}
+        />
+      </FormInputContainer>
+      <FormInputContainer>
+        <h3>Worked Until:</h3>
+        <FormInput
+          id="worked-until-input"
+          type="text"
+          value={workedUntil}
+          valueAsStr="workedUntil"
+          setData={setPracticalExperienceData}
+        />
+      </FormInputContainer>
     </section>
   );
 }
